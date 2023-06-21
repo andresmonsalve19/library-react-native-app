be111ad79581f72a52330b3002e74f5b9500271a
Jaime Andrés Monsalve Ballesteros <jmonsalveb@unal.edu.co>
2023-06-20 23:32:10 -0500

    Mi Localización y mapa implementados

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,46 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import { getCurrentPositionAsync, requestForegroundPermissionsAsync } from "expo-location";
 import {StyleSheet, Text, View} from "react-native";
-import MapView from "react-native-maps";
+import MapView, {Marker} from "react-native-maps";
 
 const Map = () => {
+    const [origin, setOrigin] = useState({
+        latitude: 6.26119,
+        longitude: -75.57886,
+    });
+    
+    useEffect(() => {
+        getLocationPermission();
+    }, []);
+    
+    async function getLocationPermission() {
+        let {status} = await requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+            alert("Permiso Denegado - No será posible obtener su ubicación");
+            return;
+        }
+        location = await getCurrentPositionAsync({});
+        const current = {
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude
+        }
+        setOrigin(current);
+    }
+    
     return (
         <View style={styles.container}>
             <Text style={styles.librayTitle}>Mi ubicación</Text>
             <MapView
                 style={styles.map}
                 region={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
+                    latitude: origin.latitude,
+                    longitude: origin.longitude,
                     latitudeDelta: 0.015,
                     longitudeDelta: 0.0121,
                 }}
-            />
+            >
+                <Marker coordinate={origin} />
+            </MapView>
         </View>
     );
 };
@@ -35,8 +61,8 @@ const styles = StyleSheet.create({
     },
     map: {
         width: "100%",
-        height: "50%"
-    }
+        height: "100%",
+    },
 });
 
 export default Map;
