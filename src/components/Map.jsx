64f53b021d4b19fe5917023d25a26b484745eda2
Jaime Andrés Monsalve Ballesteros <jmonsalveb@unal.edu.co>
2023-06-20 19:52:59 -0500

    error en Mapa

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,20 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
+import MapView from "react-native-maps";
 
 const Map = () => {
     return (
         <View style={styles.container}>
             <Text style={styles.librayTitle}>Mi ubicación</Text>
+            <MapView
+                style={styles.map}
+                region={{
+                    latitude: 37.78825,
+                    longitude: -122.4324,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+            />
         </View>
     );
 };
@@ -23,6 +33,10 @@ const styles = StyleSheet.create({
         fontWeight: "bold",
         color: "white",
     },
+    map: {
+        width: "100%",
+        height: "50%"
+    }
 });
 
 export default Map;
